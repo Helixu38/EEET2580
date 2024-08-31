11c7b2169381200285301df9a47553a8d45fc402
Helixu <104443225+Helixu38@users.noreply.github.com>
2024-09-01 00:48:03 +0700

    Add files via upload

---
+++ Week 6/StressTest/backend/src/main/java/rmitvn/SpringBootArchitecture/customer/CustomerRepository.java
@@ -0,0 +1,13 @@
+package rmitvn.SpringBootArchitecture.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Optional<Customer> findById(long id);
+}
